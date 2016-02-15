67188326274f78d4c499b5c2bc48d99c1fb719e2
jimmyfortin <jimmy.fortin@corp.ovh.com>
2016-02-15 08:59:26 -0500

    gulpfile.js update to use the new apply method

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var gulpNodeBuildTasks = require('gulp-node-build-tasks');
 
-gulpNodeBuildTasks.use(gulp);
-gulpNodeBuildTasks.registerTasks();
+var config = {};
+
+gulpNodeBuildTasks.apply(config, gulp);
